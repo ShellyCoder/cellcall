67380b6b2770a12f6618d96097aaa030004ca687
bioinformatics <49678387+ShellyCoder@users.noreply.github.com>
2021-08-13 11:35:51 +0800

    Update sankey3D.R

--- R/sankey3D.R
+++ R/sankey3D.R
@@ -20,9 +20,7 @@ LRT.Dimplot <- function(object, fontSize = 12, nodeWidth = 30,nodePadding = 5,
                         height = NULL, width = 1200, sinksRight=FALSE,
                         DIY.color = FALSE, color.DIY = NULL){
   is_myObject <- is(object, "CellInter")
-  # if(!is_myObject){
-  #   stop("object should be CellInter type.")
-  # }
+
   if(!is_myObject){
     df <- object
   }else{
@@ -39,15 +37,9 @@ LRT.Dimplot <- function(object, fontSize = 12, nodeWidth = 30,nodePadding = 5,
   df$Receptor <- paste("receiver:", df$Receptor, sep = "")
   df$TF <- paste("TF:", df$TF, sep = "")
   a <- df
-  sum_tf <- aggregate(a[,5],by=list(a$Ligand,a$Receptor),FUN=sum) # 统计每对ligand-receptor对应所有TF的和
+  sum_tf <- aggregate(a[,5],by=list(a$Ligand,a$Receptor),FUN=sum) 
   colnames(sum_tf) <- c("Ligand", "Receptor", 'Score')
-  # aa <- apply(a, 1, function(x){
-  #   tmp = dplyr::filter(sum_tf, Ligand == x[1] & Receptor == x[2])
-  #   res_tmp = as.numeric(x[1,5])/as.numeric(tmp[,3][1])
-  #
-  #   print(class(x[1,5]))
-  #   return(res_tmp)
-  # })
+
   aa <- c()
   for (i in 1:nrow(a)) {
     tmp = dplyr::filter(sum_tf, Ligand == a[i,1] & Receptor == a[i,2])
@@ -59,10 +51,6 @@ LRT.Dimplot <- function(object, fontSize = 12, nodeWidth = 30,nodePadding = 5,
   a$value <- a$weight1*a$weight2
   a <- a[,c(1,2,3,6)]
 
-  Sys.setenv(R_MAX_NUM_DLLS=999) ##Sys.setenv修改环境设置，R的namespace是有上限的，如果导入包时超过这个上次就会报错,R_MAX_NUM_DLLS可以修改这个上限  options(stringsAsFactors = F) ##options:允许用户对工作空间进行全局设置，stringsAsFactors防止R自动把字符串string的列辨认成factor
-
-  # library(networkD3)
-  # 加载数据
   mydata1 <- data.frame(ligand=as.character(a$Ligand),
                         receptor=as.character(a$Receptor),
                         tf=as.character(a$TF),
@@ -70,15 +58,10 @@ LRT.Dimplot <- function(object, fontSize = 12, nodeWidth = 30,nodePadding = 5,
   mydata.raw <- mydata1
 
   if(DIY.color){
-    color.df <- color.DIY# 输入一个如下的color对应矩阵
-    #  node   color
-    #  CCL1   #223D6C
-    #  NFKBIA #D20A13
-    #  FGF20  #FFD121
-    #  CSF2   #088247
+    color.df <- color.DIY
 
   }else{
-    # library(RColorBrewer)
+
     cellInter.color <- colorRampPalette(RColorBrewer::brewer.pal(n = 12, name = "Paired"))(24)
     ligand <- unique(mydata.raw$ligand)
     receptor <- unique(mydata.raw$receptor)
@@ -105,10 +88,10 @@ LRT.Dimplot <- function(object, fontSize = 12, nodeWidth = 30,nodePadding = 5,
   Sankeylinks <- mydata
   Sankeynodes <- data.frame(name=unique(c(Sankeylinks$source,Sankeylinks$target)))
   Sankeynodes$index <- 0:(nrow(Sankeynodes) - 1)
-  Sankeylinks <- merge(Sankeylinks,Sankeynodes,by.x="source",by.y="name")  #替换source列
-  Sankeylinks <- merge(Sankeylinks,Sankeynodes,by.x="target",by.y="name")  #替换target列
-  Sankeylinks <- merge(Sankeylinks,Sankeynodes,by.x="group",by.y="name")  #替换group列
-  Sankeydata <- Sankeylinks[,c(5,6,4,7)]  #只保留索引值列，用于绘图
+  Sankeylinks <- merge(Sankeylinks,Sankeynodes,by.x="source",by.y="name")  
+  Sankeylinks <- merge(Sankeylinks,Sankeynodes,by.x="target",by.y="name") 
+  Sankeylinks <- merge(Sankeylinks,Sankeynodes,by.x="group",by.y="name") 
+  Sankeydata <- Sankeylinks[,c(5,6,4,7)] 
   names(Sankeydata) <- c("Source","Target","Value","group")
   Sankeydata$group <- as.character(Sankeydata$group)
   Sankeyname <- Sankeynodes[,1,drop=FALSE]
