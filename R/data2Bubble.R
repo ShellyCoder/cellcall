b9c01363d23d793e07623beed80804d57c7deb84
bioinformatics <49678387+ShellyCoder@users.noreply.github.com>
2021-08-13 11:26:37 +0800

    Update data2Bubble.R

--- R/data2Bubble.R
+++ R/data2Bubble.R
@@ -7,25 +7,21 @@
 #' @export
 
 getForBubble <- function(pathway.hyper.list = pathway.hyper.list, cella_cellb){
-  ## 合并不同cc间的p值，jaccard系数和NES值
-  # library(reshape2)
+
   pathway.hyper.df <- Reduce(function(x,y) full_join(x, y, by=c("pathway")), pathway.hyper.list, accumulate =FALSE)
   rownames(pathway.hyper.df) <- pathway.hyper.df$pathway
   pathway.hyper.df$pathway <- NULL
 
-  ## 拆分后，获得不同cc间的p值
   pathway.hyper.df.pvalue <- pathway.hyper.df[, seq(from=1, to=ncol(pathway.hyper.df), by=3)]
   colnames(pathway.hyper.df.pvalue) <- cella_cellb
   rownames(pathway.hyper.df.pvalue) <- rownames(pathway.hyper.df)
   pathway.hyper.df.pvalue[is.na(pathway.hyper.df.pvalue)] <- 1
 
-  ## 拆分后，获得不同cc间的NES值
   pathway.hyper.df.NES <- pathway.hyper.df[, seq(from=3, to=ncol(pathway.hyper.df), by=3)]
   colnames(pathway.hyper.df.NES) <- cella_cellb
   rownames(pathway.hyper.df.NES) <- rownames(pathway.hyper.df)
   pathway.hyper.df.NES[is.na(pathway.hyper.df.NES)] <- 0
 
-  ## 删除不涉及的pathway
   pathway.hyper.df.NES <- pathway.hyper.df.NES[rowSums(pathway.hyper.df.NES)!=0,]
   pathway.hyper.df.pvalue <- pathway.hyper.df.pvalue[rowSums(pathway.hyper.df.NES)!=0,]
 
@@ -34,19 +30,16 @@ getForBubble <- function(pathway.hyper.list = pathway.hyper.list, cella_cellb){
   colnames(pathway.info) <- c('id', 'name', "main.object.name", "sub.object.name")
   rownames(pathway.info) <- pathway.info$id
 
-  ## melt 不同cc间的p值 适应 ggplot 绘图格式
   pathway.hyper.df.pvalue$pathway <- rownames(pathway.hyper.df.pvalue)
   pathway.hyper.df.pvalue$pathway <- pathway.info[pathway.hyper.df.pvalue$pathway, 'name']
   pvalue.melted <- melt(pathway.hyper.df.pvalue, id=c('pathway'),variable.name="cc",value.name="pvalue")
 
-  ## melt 不同cc间的NES值 适应 ggplot 绘图格式
   pathway.hyper.df.NES$pathway <- rownames(pathway.hyper.df.NES)
   pathway.hyper.df.NES$pathway <- pathway.info[pathway.hyper.df.NES$pathway, 'name']
   NES.melted <- melt(pathway.hyper.df.NES, id=c('pathway'),variable.name="cc",value.name="NES")
   NES.melted$NES[NES.melted$NES > 2]=2
   NES.melted$NES[NES.melted$NES < 0]=0
 
-  ## 合并上两步结果
   myPub.df <- inner_join(pvalue.melted, NES.melted, by=c('pathway', 'cc'))
 
   return(myPub.df)
